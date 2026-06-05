2fda02c9095b36db9e0eb18e6354add6ba150e2e
tiagooayala <tiagooayala@gmail.com>
2026-06-05 20:35:32 -0300

    Primeira versão.
    Primeira versão do montador NEANDER

---
+++ montador.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char instrucao[20];
+    int operando;
+
+    while (1) {
+
+        printf("\nDigite uma instrucao: ");
+        scanf("%s", instrucao);
+
+        if (strcmp(instrucao, "NOP") == 0) {
+            printf("Byte gerado: 00\n");
+        }
+
+        else if (strcmp(instrucao, "STA") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 10 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "LDA") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 20 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "ADD") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 30 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "OR") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 40 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "AND") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 50 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "NOT") == 0) {
+            printf("Byte gerado: 60\n");
+        }
+
+        else if (strcmp(instrucao, "JMP") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 80 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "JN") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: 90 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "JZ") == 0) {
+            scanf("%x", &operando);
+            printf("Bytes gerados: A0 %02X\n", operando);
+        }
+
+        else if (strcmp(instrucao, "HLT") == 0) {
+            printf("Byte gerado: F0\n");
+            break;
+        }
+
+        else {
+            printf("Instrucao invalida!\n");
+        }
+    }
+
+    return 0;
+}
